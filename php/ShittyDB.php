5f9f59982175f65976d0d055371f3490d43e0a08
Kyle Harrison <redactedprofile@gmail.com>
2015-08-12 18:01:01 -0700

    bug fixes

--- php/ShittyDB.php
+++ php/ShittyDB.php
@@ -17,31 +17,37 @@ class ShittyDB {
   // create/overwrite key, optionally set a ttl
   public function set($key, $value, $ttl = 0) {
     $return = file_put_contents($key, $value);
-    if($return && $ttl) $this->ttl($key, $ttl);
+    if($return && $ttl) 
+      $this->ttl($key, $ttl);
+
     return $return;
   }
 
   // if key exists, create ttl file
   public function ttl($key, $ttl) {
-    if($this->lazyCheck($key))
-      return file_put_contents("$key_ttl", strtotime("$ttl seconds from now"));
-    else
+    if($this->lazyCheck($key)) {
+      return file_put_contents($key . "_ttl", strtotime($ttl . " seconds"));
+    } else {
       return false;
+    }
   }
 
   // deletes key and ttl file
   public function delete($key) {
-    @unlink("$key_ttl")
+    @unlink($key . "_ttl");
     @unlink($key);
     return true;
   }
 
   // lazy pruner
   private function lazyCheck($key) {
-    if(file_exists("$key_ttl"))
-      if(strtotime("now") > (int)file_get_contents("$key_ttl")) 
+    if(file_exists($key . "_ttl")) {
+      if(strtotime("now") > (int)file_get_contents($key . "_ttl"))  {
         $this->delete($key);
-
+        return false;
+      }
+    }
+    
     return file_exists($key);
   }
 
--- php/test.php
+++ php/test.php
@@ -5,3 +5,19 @@ require_once('./ShittyDB.php');
 $db = new ShittyDB;
 $db->set('test', 'PHP is the best programming language evar');
 echo $db->get('test');
+
+echo "<hr />";
+echo "Expireable Test:<br /><br />";
+
+/*
+if(!$db->exists('expires')) {
+    $db->set('expires', 'Some test data that expires in 10 seconds', 10);
+}
+if(!$db->exists('expires2')) {
+    $db->set('expires2', 'Some other data set to expire in 15 seconds');
+    $db->ttl('expires2', 15);
+}
+var_dump($db->exists('expires'));
+*/
+$db->exists('expires');
+$db->exists('expires2');
